20f4e210214557e6585a3e07e06456ea28ecb212
kisdreamteam <kisdreamteam@gmail.com>
2026-05-21 16:25:48 +0700

    fix: empty-states avatar names and loactins

--- src/components/ui/EmptyState.tsx
+++ src/components/ui/EmptyState.tsx
@@ -1,10 +1,5 @@
 import Image from 'next/image';
 
-const STUDENT_MASCOT_LEFT = '/images/dashboard/EmptyStates/No-student-Left-Mascot.png';
-const STUDENT_MASCOT_RIGHT = '/images/dashboard/EmptyStates/No-student-Right-Mascot.png';
-
-const mascotImageClassName = 'h-auto w-full max-w-[200px] object-contain';
-
 const buttonClassName =
   'flex shrink-0 transform cursor-pointer rounded-lg bg-blue-600 px-8 py-4 font-semibold text-white shadow-lg transition-all duration-200 hover:scale-105 hover:bg-blue-700 hover:shadow-xl';
 
@@ -52,11 +47,11 @@ export default function EmptyState({
           <div className="mx-auto flex md:max-h-150 max-w-8xl items-center justify-center gap-1 px-1">
             <div className="hidden md:flex min-w-0 flex-1 items-center justify-end">
               <Image
-                src="/images/dashboard/Emptystates/no-student-left-mascot.png"
+                src="/images/dashboard/empty-states/no-student-left-mascot.png"
                 alt="left mascot"
                 width={200}
                 height={200}
-                className="{mascotImageClassName} md:w-200 md:h-auto"
+                className="h-auto w-full max-w-[200px] object-contain md:w-200 md:h-auto"
               />
             </div>
             <div className="flex flex-col min-w-0 w-100 flex-1 items-center justify-center">
@@ -66,11 +61,11 @@ export default function EmptyState({
             </div>
             <div className="hidden md:flex min-w-0 w-100 flex-1 items-center justify-start bg-brand-cream rounded-2xl">
               <Image
-                src="/images/dashboard/Emptystates/no-student-right-mascot.png"
+                src="/images/dashboard/empty-states/no-student-right-mascot.png"
                 alt="right mascot"
                 width={200}
                 height={200}
-                className="{mascotImageClassName} md:w-200 md:h-auto"
+                className="h-auto w-full max-w-[200px] object-contain md:w-200 md:h-auto"
               />
             </div>
           </div>
